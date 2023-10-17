2fd854df851107e642989b0503cfc313d7a5ed40
poojabhaumik <pooja@flutterflow.io>
2023-10-17 21:13:10 +0530

    Add helper methods and initialize FFLocalisation class

--- lib/src/flutter_flow/internationalization.dart
+++ lib/src/flutter_flow/internationalization.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
+//Helper class for internationalization
 class FFLocalizations {
   FFLocalizations(this.locale);
 
   final Locale locale;
 
-  static FFLocalizations of(BuildContext context) =>
-      Localizations.of<FFLocalizations>(context, FFLocalizations)!;
+  //Initializing
+  static FFLocalizations of(BuildContext context) => FFLocalizations(Locale('en'));
 
   static List<String> languages() => ['en'];
 
   String get languageCode => locale.languageCode;
-  int get languageIndex => languages().contains(languageCode)
-      ? languages().indexOf(languageCode)
-      : 0;
 
-  String getText(String key) =>
-      (kTranslationsMap[key] ?? {})[locale.languageCode] ?? '';
+  int get languageIndex => languages().contains(languageCode) ? languages().indexOf(languageCode) : 0;
+
+  String getText(String key) => (kTranslationsMap[key] ?? {})[locale.languageCode] ?? '';
 
   String getVariableText({
     String? enText = '',
@@ -29,16 +28,13 @@ class FFLocalizationsDelegate extends LocalizationsDelegate<FFLocalizations> {
   const FFLocalizationsDelegate();
 
   @override
-  bool isSupported(Locale locale) =>
-      FFLocalizations.languages().contains(locale.languageCode);
+  bool isSupported(Locale locale) => FFLocalizations.languages().contains(locale.languageCode);
 
   @override
-  Future<FFLocalizations> load(Locale locale) =>
-      SynchronousFuture<FFLocalizations>(FFLocalizations(locale));
+  Future<FFLocalizations> load(Locale locale) => SynchronousFuture<FFLocalizations>(FFLocalizations(locale));
 
   @override
   bool shouldReload(FFLocalizationsDelegate old) => false;
 }
 
-final kTranslationsMap =
-    <Map<String, Map<String, String>>>[].reduce((a, b) => a..addAll(b));
+final kTranslationsMap = <Map<String, Map<String, String>>>[].reduce((a, b) => a..addAll(b));
