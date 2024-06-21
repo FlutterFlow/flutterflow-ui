132f0fd4d8c8530de94132dc2dcf6d24feca3605
Souvik Biswas <sbis1999@gmail.com>
2024-06-21 20:52:38 +0530

    Export all themes

--- lib/flutterflow_ui.dart
+++ lib/flutterflow_ui.dart
@@ -1,6 +1,6 @@
 library flutterflow_ui;
 
-export 'src/themes/flutter_flow_theme.dart';
+export 'src/themes/flutter_flow_themes.dart';
 export 'src/utils/flutter_flow_utils.dart';
 export 'src/utils/random_data.dart';
 export 'src/widgets/flutter_flow_widgets.dart';
---
+++ lib/src/themes/flutter_flow_themes.dart
@@ -0,0 +1,5 @@
+export 'custom_color.dart';
+export 'default_dark_theme.dart';
+export 'default_light_theme.dart';
+export 'default_typography.dart';
+export 'flutter_flow_theme.dart';
