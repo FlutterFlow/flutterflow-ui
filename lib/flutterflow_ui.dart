c0270e59e42572f49ba601a38c244d03ef19320f
Souvik Biswas <sbis1999@gmail.com>
2024-06-21 20:57:46 +0530

    Change exports

--- lib/flutterflow_ui.dart
+++ lib/flutterflow_ui.dart
@@ -4,3 +4,7 @@ export 'src/themes/flutter_flow_themes.dart';
 export 'src/utils/flutter_flow_utils.dart';
 export 'src/utils/random_data.dart';
 export 'src/widgets/flutter_flow_widgets.dart';
+export 'src/themes/custom_color.dart' show CustomColor;
+export 'src/themes/default_light_theme.dart' show FFLightModeTheme;
+export 'src/themes/default_dark_theme.dart' show FFDarkModeTheme;
+export 'src/themes/default_typography.dart' show FFTypography;
